d49ce4964bde2030ec7ac5d51c01b20c0d0a1fed
Pato <99479428+patrickateng@users.noreply.github.com>
2022-03-18 21:57:41 +0300

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(inta, int b[D[D[D[D[D[D[D[D[D[D[C[C a, int b, int c);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#enfif[C[D[D[dif
+#endif
