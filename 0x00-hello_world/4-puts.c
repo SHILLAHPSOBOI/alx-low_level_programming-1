49618dad87f0850b6834966e47b1257f5f24ad14
patrickateng <99479428+patrickateng@users.noreply.github.com>
2022-03-18 17:16:57 +0300

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - entry point
+*
+*Return: 0 after printing the function
+*/
+int main(void)
+{
+puts("\"programming is like building a multilingual puzzle");
+return (0);
+}
+[A[A[A[C[C[C[C[C[C[C[C[P[B[B[D[D[D[D[D[D[D[D
