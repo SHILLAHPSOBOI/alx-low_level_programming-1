e25ebc564c5b4ae6e166add05e608a12302bbeb9
Pato <99479428+patrickateng@users.noreply.github.com>
2022-03-19 01:31:53 +0300

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,9 +4,7 @@
 *
 *Return: 0
 */
-int i:
-
-[A[A[C[C[C[C[C[C[Cmain(void)
+int main(void)
 {
 int i;
 
@@ -17,12 +15,10 @@ i = 0;
 /*
 *while (i < 10)
 *{
-*infinite loo - No increase of variable
+*infinite loop - No increase of variable
 *putchar(i);
 }
-*/ printf("infinite loop avoided! \\o/\n");
+*/
+printf("infinite loop avoided! \\o/\n");
 return (0);
 }
-[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[Cp - No increase of variable
-[B[B[C[C[C
-[A[B[B[C
