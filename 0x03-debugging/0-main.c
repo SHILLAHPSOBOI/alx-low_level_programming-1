d2753e0dc86a65a4c1b3991c6b49c4abe14999a8
patrickateng <99479428+patrickateng@users.noreply.github.com>
2022-03-18 21:53:03 +0300

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*main - main
+*Return: 0
+*/
+
+int main(void)
+{
+int:[D i:
+i = 0:
+
+positive_or_negative(i);
+return (0);[A[A[A[D[D[D[D[;[;[B[B[B[B[C[C[C[C[C
+}
