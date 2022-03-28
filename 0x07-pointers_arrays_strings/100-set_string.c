1c497193bbdd5d67bee6ed579ef3739493cbf56b
Pato <99479428+patrickateng@users.noreply.github.com>
2022-03-28 12:07:34 +0300

    Update 100-set_string.c

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,9 +2,11 @@
 
 /**
  * set_string - sets the value of a pointer to a char
- * @s: pointer to pointer we need to set to
- * @to: string to set
+ * @s: content is to be copied
+ *@to: string
+ *Return: void
  */
+
 void set_string(char **s, char *to)
 {
 	*s = to;
