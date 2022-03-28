c3f7adc9f16f65f98f81b9061fb22f78257c6c1b
Pato <99479428+patrickateng@users.noreply.github.com>
2022-03-28 12:02:19 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,49 @@
 #include "holberton.h"
-
 /**
- * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * print_times_table  -prints tables
+ *@n: print int
+ * Return: nothing
  */
 void print_times_table(int n)
 {
-	int i, j, k;
-
-	if (n >= 0 && n <= 15)
-	{
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				k = j * i;
-				if (j == 0)
-				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-			}
-			_putchar('\n');
-		}
-	}
+int a, b, c; /*Declaring statements*/
+if (n < 16 && n > -1)
+{
+for (a = 0; a <= n; a++)/*First for*/
+{
+for (b = 0; b <= n; b++)/*Secondo for*/
+{
+c = a * b;/* multiplication a*b*/
+if ((c / 10) == 0)/*If the first digit is 0*/
+{
+if  (b == 0)
+{ _putchar ('0');
 }
+if (b != 0)/*print if b is different 0*/
+{
+_putchar (' ');
+_putchar (' ');
+_putchar ((c % 10) + '0');
+}
+} /*End the first IF*/
+else if ((c / 10) <= 9 && ((c % 10) <= 9))/*print to 99*/
+{
+_putchar (' ');
+_putchar ((c / 10) + '0');
+_putchar ((c % 10) + '0');
+} /*End ELSE IF*/
+else/*print from 100*/
+{
+_putchar ((c / 100) + '0');
+_putchar (((c / 10) % 10)  + '0');
+_putchar ((c % 10) + '0');
+} /*End ELSE*/
+if (b < n) /*don't print the last (;)*/
+{
+_putchar(',');
+_putchar (' ');
+}
+} /*End second FOR*/ _putchar ('\n');
+} /*End first FOR*/
+}
+} /*End int main*/
