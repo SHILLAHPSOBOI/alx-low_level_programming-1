65405ca19510a32639f05ce4d10ec9c7d8cc3fa9
Pato <99479428+patrickateng@users.noreply.github.com>
2022-03-28 12:20:45 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#include "holberton.h"
+#include "main.h"
 /**
-* main - main
-* Return: 0
-*/
-
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
+	int i;
 
-int i;
-i = 0;
+	i = 0;
+	positive_or_negative(i);
 
-positive_or_negative(i);
-
-return (0);
+	return (0);
 }
+
