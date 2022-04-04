e6b9d283e6c7884d47fdeea5864f2a152c1bb3fa
Pato <99479428+patrickateng@users.noreply.github.com>
2022-04-04 11:40:14 +0300

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,34 +1,26 @@
-#include <stdlib.h>
 #include "holberton.h"
-
+#include <stdlib.h>
 /**
- * *create_array - creates an array of chars,
- * and initializes it with a specific char
- * @size: size of the array to create
- * @c: char to initialize the array c
- *
- * Return: pointer to the array (Success), NULL (Error)
+ * create_array - creates an array of chars and initializes it
+ * @size: size of array
+ * @c: specific character to initialize array with
+ * Return: pointer to array or NULL
  */
 char *create_array(unsigned int size, char c)
 {
-	char *p;
-	unsigned int i = 0;
+	char *a;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
-
-	p = (char *) malloc(sizeof(char) * size);
-
-	if (p == NULL)
-		return (0);
-
+	a = malloc(size * sizeof(c));
+	if (a == NULL)
+		return (NULL);
+	i = 0;
 	while (i < size)
 	{
-		*(p + i) = c;
+		*(a + i) = c;
 		i++;
 	}
-
-	*(p + i) = '\0';
-
-	return (p);
+	return (a);
 }
