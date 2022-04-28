ca69d4bfabec8ce24d46e87824961b16009cba55
Pato <99479428+patrickateng@users.noreply.github.com>
2022-04-28 17:50:48 +0300

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,40 +1,32 @@
 #include "holberton.h"
 /**
- * _strlen - returns the length of a string
- * @s: pointer to string
- *
- * Return: The length of the string
- */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c] != '\0')
-		c++;
-	return (c);
-}
-/**
- * binary_to_uint - converts a binary number to an unsigned int
- * @b: a string of 0 and 1 chars
- *
- * Return: the converted number or 0
+ * binary_to_uint - Entry Point
+ * @b: const char
+ * Return: 0
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int length = 0;
-	int power = 0;
-	int total = 0;
-	int i;
+	unsigned int res = 0;
+	int base = 1, i = 0;
 
-	if (!b)
+	if (b == NULL)
 		return (0);
-	length = _strlen(b);
-	for (i = length - 1; i >= 0; power++, i--)
+
+	while (b[i + 1])
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		if (b[i] == '1')
-			total += 1 << (power);
+		i++;
 	}
-	return (total);
+
+	while (i >= 0)
+	{
+		res += ((b[i] - '0') * base);
+		base *= 2;
+		i--;
+	}
+
+
+	return (res);
+
 }
