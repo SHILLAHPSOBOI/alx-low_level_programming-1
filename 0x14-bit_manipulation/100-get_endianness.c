7d4dceed73cf7c3b1ee07ff5853bf13abaeb4b00
Pato <99479428+patrickateng@users.noreply.github.com>
2022-04-28 13:04:15 +0300

    100-get_endianness.c

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
- * get_endianness - checks the endianness
- * Return: 0 if big endian, 1 if little endian
+ * get_endianness - Entry Point
+ * Return: 0
  */
 int get_endianness(void)
 {
-	unsigned int i = 1;
-	char *c = (char *)&i;
+	unsigned int i;
 
-	if (*c)
+	i = 1;
+	if (*(char *)&i == 1)
 		return (1);
 	else
 		return (0);
