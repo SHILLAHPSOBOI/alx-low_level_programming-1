167c01495fa118077f6e9685814f8e4e213e3697
Pato <99479428+patrickateng@users.noreply.github.com>
2022-04-28 13:05:36 +0300

    5-flip_bits.c

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,25 +1,22 @@
 #include "holberton.h"
-
 /**
- * flip_bits - counts the number of bits to change
- * to get from one number to another
- * @n: first number
- * @m: second number
- *
- * Return: number of bits to change
+ * flip_bits - Entry Point
+ * @n: input
+ * @m: bits
+ * Return: 0
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, count = 0;
-	unsigned long int current;
-	unsigned long int exclusive = n ^ m;
+	unsigned int count = 0;
 
-	for (i = 63; i >= 0; i--)
+	while (n != 0 || m != 0)
 	{
-		current = exclusive >> i;
-		if (current & 1)
+		if ((n & 1) != (m & 1))
 			count++;
+		m = m >> 1;
+		n = n >> 1;
 	}
 
 	return (count);
+
 }
