37ea4c806f822332c5d76d7682d2c87e1edd390a
Pato <99479428+patrickateng@users.noreply.github.com>
2022-07-01 13:45:36 +0300

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,41 +1,41 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: the key (can't be empty string)
+ * @value: value associated with the key (it can be empty string)
+ * Return: 1 on success 0 otherwise
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-  hash_node_t *nodes;
-  hash_node_t *new;
-  unsigned long int idx;
+	int idx;
+	hash_node_t *node;
 
-  if (ht == NULL || key == NULL)
-    return 0;
+	if (key == NULL || strlen(key) == 0 || ht == NULL)
+		return (0);
 
-  idx = key_index((const unsigned char *)key, ht->size);
-  nodes = ht->array[idx];
+	idx = key_index((const unsigned char *) key, ht->size);
 
-  while (nodes != NULL)
-    {
-      if (strcmp(nodes->key, key) == 0)
+	node = ht->array[idx];
+	while (node != NULL)
 	{
-	  free(nodes->value);
-	  nodes->value = strdup(value);
-	  if (nodes->value == NULL)
-	    return (0);
-	  return (1);
+		if (strcmp(node->key, key) == 0)
+		{
+			free(node->value);
+			node->value = strdup(value);
+			return (1);
+		}
+		node = node->next;
 	}
-      nodes = nodes->next;
-    }
 
-  new = malloc(sizeof(hash_node_t));
-  if (new == NULL)
-    return (0);
-
-  new->key = strdup(key);
-  new->value = strdup(value);
-  if (new->value == NULL)
-    return (0);
-  new->next = ht->array[idx];
-  ht->array[idx] = new;
-
-  return (1);
-  
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[idx];
+	ht->array[idx] = node;
+	return (1);
 }
